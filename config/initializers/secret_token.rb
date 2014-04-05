59709731065705f32237313c52e4a865cb0869bc
Taylor Daw <tdaw61@gmail.com>
2014-04-05 11:01:23 -0700

    what

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ManPoints::Application.config.secret_key_base = '176bccdf8b2582acf6ec559cd950ea399fd8c19ca6305920c86befdfeb0c42645ebdbfbf7e3e8c90e77f231c43cd8014b4235ce83275ae0a27442daf32230465'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ManPoints::Application.config.session_store :cookie_store, key: '_ManPoints_session'
